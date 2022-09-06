8a799ed714b8d11953e37c4d57077aafd65d4385
Sayem <sayemahamed183@gmail.com>
2022-09-06 17:25:02 +0600

    ok

--- A_Even_Odds.cpp
+++ A_Even_Odds.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main() 
 {
-    long long arr[]={1,2,3,4,5,6,7,8,9,10};
-for(int i = 0;i <1<<5; ++i)
+    long long a,b;
+    cin >>a>>b;
+    if(a%2)
+    {
+        if(a/2<b)
         {
-            for(int j = 0;j < 5;++j)
-                if(i & (1 << j))
-                    cout << arr[j] << ' ';
-            cout << endl;
+            
+        }
     }
 }
